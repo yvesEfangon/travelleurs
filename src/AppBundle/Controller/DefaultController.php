2e1a7cc3fbd777be14551766ef715bd1b2d51337
Yves Efangon <yves.efangon@gmail.com>
2017-05-20 12:58:26 +0200

    Test de ssh connect pour Git hub

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,9 +6,16 @@ use AppBundle\Form\SearchVoyageIndexType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
 
 class DefaultController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         $form   = $this->createForm(SearchVoyageIndexType::class,null,[
