c44e391adcc983c60b4db7aeb5c131829e6c84d0
Yves Efangon <yves.efangon@gmail.com>
2017-05-20 10:21:59 +0200

    BundleUser

--- src/App/UserBundle/AppUserBundle.php
+++ src/App/UserBundle/AppUserBundle.php
@@ -10,8 +10,15 @@ namespace App\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class AppUserBundle
+ * @package App\UserBundle
+ */
 class AppUserBundle extends Bundle
 {
+    /**
+     * @return string
+     */
     public function getParent()
     {
        return 'FOSUserBundle';
