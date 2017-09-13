51e3968453757dc9903403644200cb76833dc72d
Yves Efangon <yves.efangon@gmail.com>
2017-09-14 00:18:09 +0200

    Starting ElasticSearch

--- gulpfile.js
+++ gulpfile.js
@@ -34,11 +34,6 @@ gulp.task('bower', function() {
         .pipe(gulp.dest(config.bowerDir))
 });
 
-gulp.task('icons', function() {
-    return gulp.src([config.bowerDir + '/font-awesome/fonts/**.*',config.bowerDir + '/bootstrap/fonts/**.*'])
-        .pipe(gulp.dest('./src/AppBundle/Resources/public/assets/vendor/fonts'));
-});
-
 gulp.task('images', function() {
     return gulp.src('./src/AppBundle/Resources/public/images/*.*')
         .pipe(image({
@@ -52,95 +47,13 @@ gulp.task('images', function() {
             svgo: true,
             concurrent: 10
         }))
-        .pipe(gulp.dest('./src/AppBundle/Resources/public/assets/application/images'));
-});
-//
-// gulp.task('css-vendor', function() {
-//     return gulp.src(config.bowerDir + '/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
-//         .pipe(sass({
-//             style: 'compressed',
-//             loadPath: [
-//                 config.bowerDir + '/bootstrap-sass/stylesheets'
-//             ]
-//         })
-//             .on("error", notify.onError(function (error) {
-//                 return "Error: " + error.message;
-//             })))
-//         .pipe(gulp.dest('./src/AppBundle/Resources/public/assets/vendor/tmp'));
-// });
-
-// config.bowerDir + '/font-awesome/less/font-awesome.less'
-// gulp.task('css-vendor', function() {
-//     return gulp.src([config.bowerDir + '/bootstrap/less/bootstrap.less'])
-//         .pipe(less())
-//         .pipe(concat('bootstrap.css'))
-//         .pipe(gulp.dest('./src/AppBundle/Resources/public/assets/vendor/tmp'));
-// });
-
-
-gulp.task('css-vendor2', function () {
-
-    return gulp.src(config.cssPath+'/*.css')
-        .pipe(concat('vendor-1.css'))
-        .pipe(gulp.dest('src/AppBundle/Resources/public/assets/vendor/tmp'));
+        .pipe(gulp.dest('./src/AppBundle/Resources/public/images'));
 });
 
-
-gulp.task('css-application', function () {
-    return gulp.src('src/AppBundle/Resources/public/application/css/*.css')
-        .pipe(concat('application-css.css'))
-        .pipe(minify())
-        .pipe(gulp.dest('src/AppBundle/Resources/public/assets/application/css'))
-});
-
-gulp.task('css-concat', function () {
-
-    return gulp.src(['bower_components/jquery-ui/themes/base/*.css', 'src/AppBundle/Resources/public/assets/vendor/tmp/*.css'])
-        .pipe(concat('vendor-css.css'))
-        .pipe(minify())
-        .pipe(gulp.dest('src/AppBundle/Resources/public/assets/application/css'))
-    }
-);
-
-
-gulp.task('vendor-js', function () {
-    return gulp.src(
-        [
-            'src/AppBundle/Resources/public/vendor/js/jquery-2.0.0.min.js',
-           'src/AppBundle/Resources/public/vendor/jquery-ui-1.12.1.custom/jquery-ui.js',
-            'src/AppBundle/Resources/public/vendor/js/jquery.dropotron.min.js',
-            'src/AppBundle/Resources/public/js/std/bootstrap.min.js',
-            'src/AppBundle/Resources/public/vendor/js/underscore-min.js',
-            'src/AppBundle/Resources/public/vendor/js/backbone-min.js',
-            'src/AppBundle/Resources/public/vendor/js/skel.min.js',
-            'src/AppBundle/Resources/public/vendor/js/util.js',
-            'src/AppBundle/Resources/public/vendor/js/main.js',
-            'node_modules/bazinga-translator/public/js/translator.min.js'
-        ])
-        .pipe(concat('vendor-js.js'))
-        .pipe(gulp.dest('src/AppBundle/Resources/public/assets/vendor/js/'))
-});
-
-gulp.task('application-js', function () {
-    return gulp.src('src/AppBundle/Resources/public/application/js/*.js')
-        .pipe(concat('application-js.js'))
-        .pipe(gulp.dest('src/AppBundle/Resources/public/assets/vendor/tmp/'))
-});
-
-gulp.task('application-min-js', function () {
-    return gulp.src('src/AppBundle/Resources/public/assets/vendor/tmp/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('src/AppBundle/Resources/public/assets/application/js/'))
-});
-
-gulp.task('clean-tmp', function() {
-    return gulp.src('src/AppBundle/Resources/public/assets/vendor/tmp', {read: false})
-        .pipe(clean());
-});
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     gulp.watch('src/AppBundle/Resources/public/application/css/*.css', ['css']);
 });
 // 'icons', 'css-vendor2','css-concat','css-application',
-gulp.task('default', gulp.series('bower',  'vendor-js','application-js','application-min-js','clean-tmp', 'images'));
+gulp.task('default', gulp.series('images'));
